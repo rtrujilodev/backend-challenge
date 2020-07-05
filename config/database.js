9372f12e3c18d508a1df0b8026873e38eac52150
richard daros <richarddaros1@gmail.com>
2020-07-05 14:57:47 -0300

    initial commit

---
+++ config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "docker",
+    "database": "backend_challenge",
+    "host": "172.17.0.3",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "docker",
+    "database": "backend_challenge",
+    "host": "172.17.0.3",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "docker",
+    "database": "backend_challenge",
+    "host": "172.17.0.3",
+    "dialect": "mysql"
+  }
+}
